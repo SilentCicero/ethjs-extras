a8c3c1b83617b7c190fd15e23188d220428f0fc2
johncoffee <github2017@ragelse.dk>
2018-02-28 21:21:52 +0000

    write which network we're working on

--- examples/blocknumber.js
+++ examples/blocknumber.js
@@ -1,11 +1,14 @@
 const { Eth, hexToBN } = require('../');
 
-async function run (networkName = 'mainnet') {
+async function eth_blockNumber (networkName = 'mainnet') {
   const asHex = await Eth({ network: networkName }).raw('eth_blockNumber')
   const asNumber = hexToBN(asHex).toString(10)
   console.log(`Block number: ${asNumber}`)
 }
 
-run(process.argv[2])
+let networkName = process.argv[2] || 'mainnet'
+console.log(`Calling eth_blockNumber on ${networkName}.`)
+
+eth_blockNumber(networkName)
   .then(() => console.log('Done.'))
   .catch(console.log);
